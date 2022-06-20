3ae0f17399d25945107af47d80862c7b54dbdf9f
Akash Yadav <itsaky01@gmail.com>
2022-06-20 17:23:29 +0530

    Feature: Ability to filter logs

--- app/src/main/java/com/itsaky/androidide/adapters/LogLinesAdapter.kt
+++ app/src/main/java/com/itsaky/androidide/adapters/LogLinesAdapter.kt
@@ -22,21 +22,34 @@ import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
 import android.util.TypedValue
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.DiffUtil.Callback
 import androidx.recyclerview.widget.RecyclerView
 import com.blankj.utilcode.util.SizeUtils
 import com.itsaky.androidide.adapters.LogLinesAdapter.VH
 import com.itsaky.androidide.models.LogLine
+import com.itsaky.androidide.utils.ILogger
+import com.itsaky.androidide.utils.ILogger.Priority
+import com.itsaky.androidide.utils.ILogger.Priority.ERROR
+import com.itsaky.androidide.utils.ILogger.Priority.INFO
+import com.itsaky.androidide.utils.ILogger.Priority.VERBOSE
+import com.itsaky.androidide.utils.ILogger.Priority.WARNING
 import com.itsaky.androidide.utils.TypefaceUtils
 
 /**
  * Adapter for the log view.
  * @author Akash Yadav
  */
-class LogLinesAdapter(private val lines: MutableList<LogLine> = mutableListOf()) :
-    RecyclerView.Adapter<VH>() {
+class LogLinesAdapter : RecyclerView.Adapter<VH>() {
 
+    private val log = ILogger.newInstance(javaClass.simpleName)
+    private val lines: MutableList<LogWrapper> = mutableListOf()
+    private var shown = lines
+    private var isFiltered = false
+    private var query: Query? = null
     var simpleFormatting = true
 
     companion object {
@@ -52,27 +65,77 @@ class LogLinesAdapter(private val lines: MutableList<LogLine> = mutableListOf())
     }
 
     fun add(line: LogLine) {
-        this.lines.add(line)
-        this.notifyItemInserted(this.lines.lastIndex)
+        val wrapped = wrap(line)
+        this.lines.add(wrapped)
+        if (isFiltered && matchesFilter(wrapped, query!!.query, query!!.priority)) {
+            shown.add(wrapped)
+            notifyItemInserted(this.shown.lastIndex)
+        } else {
+            notifyItemInserted(this.lines.lastIndex)
+        }
 
         if (this.lines.size > MAX_LINES) {
             this.lines.removeAt(0)
+            if (isFiltered && this.shown[0] == this.lines[0]) {
+                this.shown.removeAt(0)
+            }
             this.notifyItemRemoved(0)
         }
     }
 
-    fun getItem(index: Int) = this.lines[index]
+    @SuppressLint("NotifyDataSetChanged")
+    fun filter(query: String, priority: Priority) {
+        this.query = Query(query, priority)
+        val newList = this.lines.filter { matchesFilter(it, query, priority) }
+        val result =
+            DiffUtil.calculateDiff(
+                object : Callback() {
+                    override fun getOldListSize(): Int {
+                        return lines.size
+                    }
 
-    fun getAsString() =
+                    override fun getNewListSize(): Int {
+                        return newList.size
+                    }
+
+                    override fun areItemsTheSame(
+                        oldItemPosition: Int,
+                        newItemPosition: Int
+                    ): Boolean {
+                        return lines[oldItemPosition] == newList[newItemPosition]
+                    }
+
+                    override fun areContentsTheSame(
+                        oldItemPosition: Int,
+                        newItemPosition: Int
+                    ): Boolean {
+                        return areItemsTheSame(oldItemPosition, newItemPosition)
+                    }
+                })
+        shown = newList.toMutableList()
+        this.isFiltered = shown.size != lines.size
+        if (!isFiltered) {
+            this.shown = lines
+            this.query = null
+            notifyDataSetChanged()
+        } else {
+            result.dispatchUpdatesTo(this)
+        }
+    }
+
+    fun matchesFilter(wrapper: LogWrapper, query: String, priority: Priority): Boolean {
+        val line = wrapper.log
+        return (query.isBlank() || lineString(line).contains(query, ignoreCase = true)) &&
+            (line.priority == priority || priority == VERBOSE)
+    }
+
+    fun getLine(index: Int) = this.lines[index].log
+
+    fun allAsString() =
         StringBuilder()
             .apply {
                 for (line in lines) {
-                    append(
-                        if (simpleFormatting) {
-                            line.toSimpleString()
-                        } else {
-                            line.toString()
-                        })
+                    append(lineString(line))
                     append("\n")
                 }
             }
@@ -84,53 +147,75 @@ class LogLinesAdapter(private val lines: MutableList<LogLine> = mutableListOf())
         text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 11f)
         text.setPaddingRelative(dp1, dp1 / 2, dp1, dp1 / 2)
         text.setTextIsSelectable(true)
+        text.maxLines = 1
         text.typeface = TypefaceUtils.jetbrainsMono()
         return VH(text)
     }
 
     override fun onBindViewHolder(holder: VH, position: Int) {
-        val line = getItem(position)
-        val spanned =
-            if (line is LogWrapper) {
-                line.spanned!!
-            } else {
-                val spannable =
-                    SpannableString(
-                        if (simpleFormatting) line.toSimpleString() else line.toString())
-                val color =
-                    when (line.priority) {
-                        LogLine.ERROR -> -0xbbcca
-                        LogLine.WARNING -> -0x14c5
-                        LogLine.INFO -> -0xb350b0
-                        else -> -0xa0a0b
-                    }
-                spannable.setSpan(
-                    ForegroundColorSpan(color),
-                    0,
-                    spannable.length,
-                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-                lines[position] = LogWrapper(line).apply { spanned = spannable }
-                spannable
-            }
+        val line = shown[position]
+        if (!line.visible) {
+            holder.itemView.visibility = View.GONE
+            return
+        } else {
+            holder.itemView.visibility = View.VISIBLE
+        }
+
+        val spanned = line.spannable
         holder.text.text = spanned
     }
 
-    override fun getItemCount() = this.lines.size
+    override fun getItemCount() = this.shown.size
 
-    class LogWrapper(val log: LogLine) : LogLine() {
+    private fun wrap(line: LogLine): LogWrapper {
+        val spannable = SpannableString(lineString(line))
+        val color =
+            when (line.priority) {
+                ERROR -> -0xbbcca
+                WARNING -> -0x14c5
+                INFO -> -0xb350b0
+                else -> -0xa0a0b
+            }
+        spannable.setSpan(
+            ForegroundColorSpan(color), 0, spannable.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        return LogWrapper(line, spannable).apply { visible = true }
+    }
 
-        var spanned: Spannable? = null
-
-        override fun equals(other: Any?): Boolean {
-            return this.log == other
+    private fun lineString(logLine: LogLine) =
+        if (simpleFormatting) {
+            logLine.toSimpleString()
+        } else {
+            logLine.toString()
         }
 
-        override fun hashCode(): Int {
-            return this.log.hashCode()
-        }
+    class LogWrapper(val log: LogLine, val spannable: Spannable) : LogLine() {
+
+        var visible = true
 
         override fun toString() = this.log.toString()
         override fun toSimpleString(): String = this.log.toSimpleString()
         override fun formattedTagAndMessage(): String = this.log.formattedTagAndMessage()
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as LogWrapper
+
+            if (log != other.log) return false
+            if (spannable != other.spannable) return false
+            if (visible != other.visible) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = log.hashCode()
+            result = 31 * result + spannable.hashCode()
+            result = 31 * result + visible.hashCode()
+            return result
+        }
     }
+
+    data class Query(val query: String, val priority: Priority)
 }
--- app/src/main/java/com/itsaky/androidide/fragments/LogViewFragment.kt
+++ app/src/main/java/com/itsaky/androidide/fragments/LogViewFragment.kt
@@ -18,15 +18,21 @@
 package com.itsaky.androidide.fragments
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.itsaky.androidide.adapters.LogLinesAdapter
 import com.itsaky.androidide.databinding.FragmentLogBinding
 import com.itsaky.androidide.models.LogLine
 import com.itsaky.androidide.utils.ILogger
+import com.itsaky.androidide.utils.ILogger.Priority
+import com.itsaky.androidide.utils.SingleTextWatcher
 
 /**
  * Fragment to show logs in a [androidx.recyclerview.widget.RecyclerView].
@@ -37,7 +43,7 @@ abstract class LogViewFragment : Fragment() {
     private val log = ILogger.newInstance(javaClass.simpleName)
     var binding: FragmentLogBinding? = null
     var adapter: LogLinesAdapter? = null
-    
+
     fun appendLog(line: LogLine) {
         if (this.binding == null || this.adapter == null) {
             return
@@ -48,16 +54,16 @@ abstract class LogViewFragment : Fragment() {
             this.adapter!!.add(line)
         }
     }
-    
+
     abstract fun getLogType(): String
-    
+
     abstract fun isSimpleFormattingEnabled(): Boolean
 
-    protected open fun logLine(priority: Int, tag: String, message: String) {
-        val line = LogLine(ILogger.priorityChar(priority), tag, message)
+    protected open fun logLine(priority: Priority, tag: String, message: String) {
+        val line = LogLine(priority, tag, message)
         appendLog(line)
     }
-    
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -66,14 +72,47 @@ abstract class LogViewFragment : Fragment() {
         binding = FragmentLogBinding.inflate(inflater, container, false)
         return binding!!.root
     }
-    
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val manager = LinearLayoutManager(requireContext())
         this.binding!!.lines.layoutManager = manager
-        
+
         this.adapter = LogLinesAdapter()
         this.binding!!.lines.adapter = this.adapter
+
+        this.binding!!
+            .searchField
+            .addTextChangedListener(
+                object : SingleTextWatcher() {
+                    override fun afterTextChanged(s: Editable) {
+                        val text = s.toString().trim()
+                        val priority =
+                            Priority.values()[binding!!.priorityFilter.selectedItemPosition]
+                        adapter!!.filter(text, priority)
+                    }
+                })
+
+        this.binding!!.priorityFilter.adapter =
+            ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_spinner_dropdown_item,
+                Priority.values().map { it.name })
+        this.binding!!.priorityFilter.onItemSelectedListener =
+            object : OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    val text = binding!!.searchField.text.toString().trim()
+                    val priority = Priority.values()[position]
+                    adapter!!.filter(text, priority)
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+            }
     }
 
     override fun onDestroy() {
